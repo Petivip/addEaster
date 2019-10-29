9274ec1d852fe9ba1947f4f7c95a4dcf5d34558b
petivip <petivip@gmail.com>
2019-10-29 16:34:02 -0400

    add egg

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static HashMap<String, String> myMap = new HashMap<String, String>();
+    public static boolean capsLock=false;
+    public static String color="white";
     public static void main(String[] args) {
         Scanner myVar = new Scanner(System.in);
         System.out.println("Good Morning? what is your problem today?Enter your response here or press q to quite.");
         String answer = myVar.nextLine();
         wordSwitch(answer);
+
 
         if(answer.equalsIgnoreCase("q")){
             quit();
@@ -35,36 +38,76 @@ public class Main {
             System.out.println("Bye bye!!! See you next time.");
 
         }
+
+    public static void pig(String word) {
+        char ch = word.charAt(0);
+        if(word.equalsIgnoreCase("caps lock")){
+            if(!capsLock){
+                capsLock=true;
+            }else {
+                capsLock=false;
+            }
+        }
+              if(capsLock){
+
+                  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == ' ') {
+                      System.out.println((word + "way").toUpperCase());
+                  } else {
+                      System.out.println((word + "ay").toUpperCase());
+                  }
+              }else {
+
+                  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == ' ') {
+                      System.out.println(word + "way");
+                  } else {
+                      System.out.println(word + "ay");
+                  }
+              }
+
+    }
+
     public static void wordSwitch(String sentence) {
-        String newS=sentence;
-        myMap.put("i","you");
-        myMap.put("me","you");
-        myMap.put("am","are");
-        myMap.put("my","your");
-       for(String word:sentence.split(" ")){
-           for(String key:myMap.keySet()) {
-               if (word.equalsIgnoreCase(key)) {
-                   newS = newS.replace(word, myMap.get(word));
-               }
+        Scanner myVar=new Scanner(System.in);
+        String newS = sentence;
+        myMap.put("i", "you");
+        myMap.put("me", "you");
+        myMap.put("am", "are");
+        myMap.put("my", "your");
+
+        if (sentence.equalsIgnoreCase("pig")) {
+            System.out.println("Switching to a pig game latin game");
+            String input2="pig";
+           while(!input2.equalsIgnoreCase("q")) {
+
+               System.out.println("What word do want to have pigmitized?:");
+               input2 = myVar.nextLine();
+               pig(input2);
            }
-       }
-        Random r=new Random();
-        int random1=r.nextInt(2);
-        int random2=r.nextInt(3);
+           quit();
+        } else {
+            for (String word : sentence.split(" ")) {
+                for (String key : myMap.keySet()) {
+                    if (word.equalsIgnoreCase(key)) {
+                        newS = newS.replace(word, myMap.get(word));
+                    }
+                }
+            }
+            Random r = new Random();
+            int random1 = r.nextInt(2);
+            int random2 = r.nextInt(3);
 
 
-       String [] hedges={"Please tell me more ","Many of my patients tell me the same thing ", "It is getting late, maybe we had better quit "};
+            String[] hedges = {"Please tell me more ", "Many of my patients tell me the same thing ", "It is getting late, maybe we had better quit "};
 
-       String[] qualifiers={"Why do you say that ", "You seem to think that ","So, you are concerned that "};
-        if( random1==1){
-            System.out.println(hedges[random2]);
-        }else{
-            System.out.println( qualifiers[random2]+ newS);
+            String[] qualifiers = {"Why do you say that ", "You seem to think that ", "So, you are concerned that "};
+            if (random1 == 1) {
+                System.out.println(hedges[random2]);
+            } else {
+                System.out.println(qualifiers[random2] + newS);
+            }
+
+
         }
-
-
-
-        }
-
+    }
     }
 
